0fe3687a7dc3a2485aebac26f2ad08ad6a60c48f
Njike Njiah Abednego <njikeabednego@hotmail.com>
2024-07-30 02:13:38 +0100

    All tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A python script that,  using Rest Api, for
+a given employee ID, returns information about
+his/her TODO list program
+"""
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    total = 0
+    tasks = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
+    employee = ""
+
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
+
+            for i in data:
+                if i.get('userId') == int(argv[1]):
+                    total += 1
+                    if i.get('completed') is True:
+                        completed += 1
+                        tasks.append(i.get('title'))
+            print("Employee {} is done with tasks({}/{}):".format
+                  (employee, completed, total))
+
+            for i in tasks:
+                print("\t {}".format(i))
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+This Readme Contains Projects files regarding API for ALX
